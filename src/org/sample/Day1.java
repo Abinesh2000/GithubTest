fc87d8b9fa7db4f1489ec36e9be1ac59d8fe9af7
Abinesh2000 <abidheeran3012@gmail.com>
2022-11-25 08:18:17 +0530

    A added few lines

--- src/org/sample/Day1.java
+++ src/org/sample/Day1.java
@@ -2,8 +2,12 @@ package org.sample;
 
 public class Day1 {
 
+	private void m1() {
+		System.out.println("Method 1 Add form A");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("start");
+		System.out.println("Endes..........");
 	}
 }
